2137ec18201ec5c6190d51460153db5b5f94bcb8
JakePIXL <jakewjevans@gmail.com>
2023-12-10 21:47:49 -0500

    fixed download and search box

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -102,9 +102,9 @@ export default {
       },
       colors: {
         // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: colors.indigo,
+        accent: accent,
         // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: colors.gray,
+        gray: gray,
       },
       fontFamily: {
         // Your preferred text font. Starlight uses a system font stack by default.
